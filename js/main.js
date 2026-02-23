67bd83d1ab240d29f43c50952ee2c40684350b39
Ilija Zikic <zikicilija@gmail.com>
2026-02-23 15:25:44 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const products = {
+  id: 1,
+  name: "Lolito",
+  description: "Stylish cafe chair",
+  price: 2500000,
+  discount: 30,
+  newCollection: false,
+  category: "Dinning",
+  descriptionMedium:
+    "Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound.",
+  descriptionLong: [
+    "Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.",
+    "Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.",
+  ],
+  additionInfo: {
+    info1: "Lorem",
+    info2: "Lorem lorem",
+    info3: "Lorem lorem lorem",
+  },
+  reviews: ["One,Two, Three"],
+  compararison: {
+    general: {
+      salesPackage: "1 sectional sofa",
+      modelNumber: "TFCBLIGRBL6SRHS",
+      secondaryMaterial: "Solid Wood",
+      configuration: "L-shaped",
+      upholsterMaterial: "Fabric + Cotton",
+      upholsteryColor: "Bright Grey & Lion",
+    },
+    product: {
+      filingMaterijal: "Foom",
+      finishType: "Bright Grey & Lion",
+      adjustableHeadrest: "No",
+      maximumLoadCapacity: "280 KG",
+      originOfManufacture: "India",
+    },
+    dimension: {
+      width: 265.32 + "cm",
+      height: 76 + "cm",
+      weight: 45 + "KG",
+      seatHeight: 41.52 + "cm",
+      legHeight: 5.46 + "cm",
+    },
+    warranty: {
+      warrantySummary: "1 Year Manufacturing Warranty",
+      warrantyServiceType:
+        "For Warranty Claims or Any Product Related Issues Please Email at operations@trevifurniture.com",
+      coveredInWarranty: "Warranty Against Manufacturing Defect",
+      notCoveredInWarranty:
+        "The Warranty Does Not Cover Damages Due To Usage Of The Product Beyond Its Intended Use And Wear & Tear In The Natural Course Of Product Usage.",
+      domesticWarranty: "1 Year",
+    },
+  },
+};
